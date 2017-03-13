b36a2bed7ea3067c899874e558b61938460dfbea
Claire Nessler <clrnessler@gmail.com>
2017-03-12 21:00:35 -0600

    initial commit for playground

---
+++ src/main/java/com/example/PagesController.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class PagesController {
+	
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "HELLO WORLD";
+	}
+	
+}
